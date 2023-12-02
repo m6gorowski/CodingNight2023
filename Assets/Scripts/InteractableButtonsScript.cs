eb9a7fdd2c4183ebf4e515e67da1a5716616af4e
m6gorowski <m6gorowski@protonmail.com>
2023-12-02 07:11:31 +0100

    Upgrades work now

--- Assets/Scripts/InteractableButtonsScript.cs
+++ Assets/Scripts/InteractableButtonsScript.cs
@@ -1,29 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using TMPro;
 using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class InteractableButtonsScript : MonoBehaviour
 {
+    public TMP_Dropdown section;
     private int sectionId;
-    public SectionManagementScript SectionManagement;
+    private SectionManagementScript SectionManagement;
     private TrashScript _trashScript;
     private MinionScript _minionScript;
     public int ShopItemLevelNumber;
     public int ShopItemNumber;
     public int Cost;
+    public TextMeshProUGUI Cost1;
+    private bool canBebought = true;
 
-    private void Awake()
-    {
-        _minionScript = SectionManagement.transform.GetChild(1).GetComponent<MinionScript>();
-        _trashScript = SectionManagement.transform.GetChild(0).GetComponent<TrashScript>();
-    }
+    
 
 
     private void Update()
     {
-        
+        if(section.options[section.value].text == "Sekcja 1")
+        {
+            sectionId = 0;
+            SectionManagementScript[] sections = FindObjectsOfType<SectionManagementScript>();
+            foreach(SectionManagementScript section in sections)
+            {
+                if(section.Id == sectionId)
+                {
+                    SectionManagement = section;
+                    _minionScript = SectionManagement.transform.GetChild(1).GetComponent<MinionScript>();
+                    _trashScript = SectionManagement.transform.GetChild(0).GetComponent<TrashScript>();
+                }
+            }
+            
+        }
+        else if(section.options[section.value].text == "Sekcja 2")
+        {
+            sectionId = 1;
+            SectionManagementScript[] sections = FindObjectsOfType<SectionManagementScript>();
+            foreach (SectionManagementScript section in sections)
+            {
+                if (section.Id == sectionId)
+                {
+                    SectionManagement = section;
+                    _minionScript = SectionManagement.transform.GetChild(1).GetComponent<MinionScript>();
+                    _trashScript = SectionManagement.transform.GetChild(0).GetComponent<TrashScript>();
+                }
+            }
+        }
         CheckCost();   
     }
     public void BuyItem()
@@ -34,15 +63,30 @@ public class InteractableButtonsScript : MonoBehaviour
         {
             SectionManagement.saves[2] = ShopItemLevelNumber -1;
             SectionManagement.saves[3] = ShopItemLevelNumber -1;
+            
         }
         _trashScript.setAllValues();
         _minionScript.setAllValues();
         Cost *= 3;
+        Cost1.text = Cost.ToString() + "$";
         ShopItemLevelNumber++;
+        if(SectionManagement.saves[ShopItemNumber] == null)
+        {
+            Button[] buttons = FindObjectsOfType<Button>();
+            foreach (Button btn in buttons)
+            {
+                if (btn.name == this.name)
+                {
+                    btn.interactable = false;
+                    canBebought = false;
+                    Cost1.text = "MAX";
+                }
+            }
+        }
     }
     public void CheckCost()
     {
-        if (Cost <= SectionManagement.money)
+        if (Cost <= SectionManagement.money && canBebought)
         {
             Button[] buttons = FindObjectsOfType<Button>();
             foreach(Button btn in buttons)
@@ -54,7 +98,7 @@ public class InteractableButtonsScript : MonoBehaviour
             }
             
         }
-        else if(Cost > SectionManagement.money)      
+        else if(Cost > SectionManagement.money && canBebought)      
         {
             Button[] buttons = FindObjectsOfType<Button>();
             foreach (Button btn in buttons)
